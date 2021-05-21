15e674ff8d0907991017405cae8461e7c6821efb
keemeesuu <kimhogy@gmail.com>
2021-05-21 14:45:35 +0900

    Initial Commit

---
+++ SwiftUI_stact_layout_tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_stact_layout_tutorial
+//
+//  Created by hogy on 2021/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_stact_layout_tutorial/SwiftUI_stact_layout_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_stact_layout_tutorialApp.swift
+//  SwiftUI_stact_layout_tutorial
+//
+//  Created by hogy on 2021/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_stact_layout_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
